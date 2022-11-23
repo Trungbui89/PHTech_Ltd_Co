34f42fc28a18ccdcdc06b91836f7b6436cd38984
Trungbui89 <trungbui19894@gmail.com>
2022-11-24 00:44:04 +0700

    add carbon lib

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import "carbon-components-svelte/css/all.css";
+    import { Theme, Content } from "carbon-components-svelte";
+
+    let theme = "white";
+</script>
+
+<Theme bind:theme />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Content>
+    <slot />
+</Content>
