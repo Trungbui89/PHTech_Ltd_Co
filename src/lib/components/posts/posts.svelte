4ec73ede88246257a3958809ddcf053c4256a4fb
Trungbui89 <trungbui19894@gmail.com>
2022-11-25 15:57:38 +0700

    add post component

---
+++ src/lib/components/posts/posts.svelte
@@ -0,0 +1,22 @@
+<div id="posts">
+    <header>
+        <h2>Recent Blog Posts</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+    </header>
+</div>
+
+<style lang="scss">
+    @use 'src/variables';
+
+    #posts {
+        color: variables.$paragraph-color;
+        header {
+            text-align: center;
+            margin-bottom: 60px;
+            h2 {
+                font-weight: 600;
+                color: variables.$h2-color;
+            }
+        }
+    }
+</style>
